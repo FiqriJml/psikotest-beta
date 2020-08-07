087ef13f9a278b6eb39726a925745a6369fa79f7
fiqrijml <jamalfiqri@gmail.com>
2020-08-07 14:53:34 +0800

    tambahkan fitur auto resize textarea

--- src/components/tests/soal/AddSoalForm.js
+++ src/components/tests/soal/AddSoalForm.js
@@ -18,29 +18,31 @@ function AddSoalForm({match}) {
                 <div className="form-group row">
                 <label htmlFor="soal" className="col-sm-2 col-lg-1 col-form-label">Soal</label>
                     <div className="col-sm-10 col-lg-11">
-                        <textarea style={{height: "auto"}} onChange={onEnter} id="soal" className="form-control"></textarea>
+                        <textarea onChange={onEnter} id="soal" className="form-control"></textarea>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="pilihan_a" className="col-sm-2 col-lg-1 col-form-label">Pilihan A</label>
                     <div className="col-sm-10 col-lg-5">
-                        <textarea id="pilihan_a" className="form-control" rows="1"></textarea>
+                        <textarea onChange={onEnter} id="pilihan_a" className="form-control" rows="1"></textarea>
                     </div> <br/> <br/>
                     <label htmlFor="pilihan_b" className="col-sm-2 col-lg-1 col-form-label">Pilihan B</label>
                     <div className="col-sm-10 col-lg-5">
-                        <textarea id="pilihan_b" className="form-control" rows="1"></textarea>
+                        <textarea onChange={onEnter} id="pilihan_b" className="form-control" rows="1"></textarea>
                     </div> <br/> <br/>
+
                     <label htmlFor="pilihan_c" className="col-sm-2 col-lg-1 col-form-label">Pilihan C</label>
                     <div className="col-sm-10 col-lg-5">
-                        <textarea id="pilihan_c" className="form-control" rows="1"></textarea>
+                        <textarea onChange={onEnter} id="pilihan_c" className="form-control" rows="1"></textarea>
                     </div> <br/> <br/>
                     <label htmlFor="pilihan_d" className="col-sm-2 col-lg-1 col-form-label">Pilihan D</label>
                     <div className="col-sm-10 col-lg-5">
-                        <textarea id="pilihan_d" className="form-control" rows="1"></textarea>
+                        <textarea onChange={onEnter} id="pilihan_d" className="form-control" rows="1"></textarea>
                     </div> <br/> <br/>
+                    
                     <label htmlFor="pilihan_e" className="col-sm-2 col-lg-1 col-form-label">Pilihan E</label>
                     <div className="col-sm-10 col-lg-5">
-                        <textarea id="pilihan_e" className="form-control" rows="1"></textarea>
+                        <textarea onChange={onEnter} id="pilihan_e" className="form-control" rows="1"></textarea>
                     </div> <br/> <br/>
                 </div>
                 <button className="btn btn-primary float-right">Submit {contoh} Soal</button>
